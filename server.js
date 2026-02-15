85dee34350a17bf0c7111448c651c5096787e5e8
Harsh Shukla <harshshukla24112000@gmail.com>
2026-02-15 17:21:22 +0530

    Update server route

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const express = require("express")
+ 
+//rest object
 
+const app = express()
+
+//middleware
+
+app.use(express.json())
+
+//routes
+app.get("/test" , (req,res)=>{
+    res.status(200).send("<h1> welcome to node server</h1>")
+})
+
+
+//port
+const PORT= 8080
+
+
+//listen
+app.listen(PORT,()=>{
+    console.log("Node Server running")
+})
