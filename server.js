cb53170e12c4daf70d55233921053f877d0e3274
Harsh Shukla <harshshukla24112000@gmail.com>
2026-02-16 23:40:14 +0530

    packages

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require("express")
+const morgan  = require("morgan")
+const dotenv = require("dotenv")
+
+//env config
+
+dotenv.config()
  
 //rest object
 
@@ -7,6 +13,7 @@ const app = express()
 //middleware
 
 app.use(express.json())
+app.use(morgan("dev"))
 
 //routes
 app.get("/test" , (req,res)=>{
@@ -15,10 +22,10 @@ app.get("/test" , (req,res)=>{
 
 
 //port
-const PORT= 8080
+const PORT= process.env.PORT || 8000
 
 
 //listen
 app.listen(PORT,()=>{
-    console.log("Node Server running")
+    console.log(`Node Server running on ${process.env.DEV_MODE} node on port no ${PORT}`)
 })
